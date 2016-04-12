f630138ea442acfec4f0c7d8199728fb4c24a2a5
Ben Frey <spartanhooah@users.noreply.github.com>
2016-04-12 14:33:12 -0400

    Reconstruction working.

--- PCA_exp.m
+++ PCA_exp.m
@@ -5,8 +5,8 @@ p_vals = [10, 50, 100, 200];
 errors = zeros(size(p_vals));
 
 for p = p_vals
-    reconstructed = (score(:, 1:p) * coeff(1:p)')';
-    reconstructed = reshape(reconstructed, 16, 16);
-    imshow(reconstructed);
+    reconstructed = score(:, 1:p) * coeff(:, 1:p)';
+    reconstructed = reshape(reconstructed(2, :), 16, 16);
+    imshow(reconstructed');
     pause
 end
