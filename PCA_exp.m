29c4d358e041af96e83f40b7f91df0bc2c4cf60f
Ben Frey <spartanhooah@gmail.com>
2016-04-12 17:56:57 -0400

    Added code to plot multiple figures.

--- PCA_exp.m
+++ PCA_exp.m
@@ -3,10 +3,17 @@ load('USPS.mat');
 [coeff, score] = pca(A, 'Centered', 'off');
 p_vals = [10, 50, 100, 200];
 errors = zeros(size(p_vals));
+figures = [];
 
 for p = p_vals
     reconstructed = score(:, 1:p) * coeff(:, 1:p)';
     reconstructed = reshape(reconstructed(2, :), 16, 16);
-    imshow(reconstructed');
-    pause
+    figures = [figures, reconstructed'];
+end
+
+for i = 1:4
+    subplot(1, 4, i)
+    l_index = (i - 1) * 16 + 1;
+    r_index = l_index + 15;
+    imshow(figures(:, l_index:r_index))
 end
